63442b861a7a3c3507a5a7c22b490b27ff66d619
Tanya Saroha <tanya@joshsoftware.com>
2018-02-05 23:01:07 +0530

    [Tanya] Message model Created

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180205172205) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "sender_id"
+    t.text "content"
+    t.integer "read"
+    t.integer "like"
+    t.integer "unlock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
