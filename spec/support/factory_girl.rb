d5eb290737a729a0f97e11fa7ead9273d41f75f6
priyanka yadav <priyanka216668@gmail.com>
2018-02-06 00:13:47 +0530

    [priyanka] database-cleaner added to gemfile

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,3 +1,19 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
+
+RSpec.configure do |config|
+  # ... other FactoryGirl configs
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+end
